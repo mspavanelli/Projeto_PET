35081ddc0a1a23860fa750f5efcaa4b1ab250250
Matheus Pavanelli <mspavanelli@gmail.com>
2015-05-20 11:33:33 -0300

    Primeiros Passos

--- modelo.c
+++ modelo.c
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+// #include <conio.h>
 #include <malloc.h>
 #include <string.h>
 #include <ctype.h>
@@ -23,9 +23,6 @@ char* nroUSP2() {
     return("9065701");
 }
 
-
-
-
 void ordenar(char *nomearq);
 
 
@@ -34,23 +31,62 @@ void ordenar(char *nomearq);
 // e nao use vetores ou listas adicionais
 int M[100];
 
+/* Funções Auxiliares*/
+void imprimir_vetor()
+{
+	int i;
+	for( i = 0; i < 100; i++ )
+	{
+		printf("%d ", M[i] );
+	}
+	printf("\n");
+}
 
+void gravar_arquivo( FILE * destino )
+{
+	int i;
+	for ( i = 0; i < 100; i++ )
+	{
+		fprintf(destino, "%i ", M[i]);
+	} 
+}
 
-
+/* Ordenação Interna */
+void sort()
+{
+	int i, j;
+	for ( i = 0; i < 100; i++ )
+	{
+		for ( j = i; j < 100; j++ )
+		{
+			if ( M[j] < M[i] )
+			{
+				int aux = M[j];
+				M[j] = M[i];
+				M[i] = aux;
+			}
+		}
+	}
+}
 
 //------------------------------------------
 // O EP consiste em implementar esta funcao
 //------------------------------------------
-void ordenar(char *nomearq) {
-
-
-	// seu codigo AQUI
+void ordenar(char *nomearq)
+{
+	FILE * arquivo = fopen( nomearq, "r" );		// arquivo ser ordenado
+	FILE * saida = fopen( "saida.txt", "w" );	// arquivo ordenado
+	int i;	// variável de iteração
+	
+	while( !feof(arquivo) )	// enquanto "houver arquivo"
+	{	
+		for ( i = 0; i < 100; i++ )	// percorre todo o arquivo
+			fscanf(arquivo, "%i", &M[i]);	// preenche a memória com os dados
+		sort();	// ordena a memória
+		gravar_arquivo(saida);	// grava dados ordenados no arquivo de saída
+	}
 
 }
-
-
-
-
 
 
 //---------------------------------------------------------
@@ -62,7 +98,7 @@ int main() {
 	
 
 	// serao realizadas chamadas como esta:
-	// ordenar(arqteste);
+	ordenar(arqteste);
 
 
 }
