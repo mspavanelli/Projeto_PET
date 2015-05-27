9303132225ed521436df615274ec57c9d8113abd
Matheus Pavanelli <mspavanelli@gmail.com>
2015-05-27 11:12:14 -0300

    Decio Rules

--- modelo.c
+++ modelo.c
@@ -45,6 +45,13 @@ void gravar_arquivo( FILE * destino, int limite )
 		fprintf(destino, "%i ", M[i]); 
 }
 
+void gravar_arquivo_tmp( FILE * destino, int limite )
+{
+	int i;
+	for ( i = 0; i < limite; i++ )
+		fwrite( &M[i], sizeof(int), 1, destino); 
+}
+
 /* Ordenação Interna usando SelectionSort */
 void sort( int limite )
 {
@@ -71,10 +78,52 @@ void binToBin( FILE * source, FILE * target )
 		fwrite(  &aux, sizeof(int), 1, target );
 }
 
-/* Junta conteúdo de f1 e f2 em f2 em ordem crescente */
-void merge( FILE * f1, FILE * f2 )
-{
+void binToTxt(){}
 
+/* Junta conteúdo de f1 e f2 em f3 em ordem crescente */
+void merge( FILE * f1, FILE * f2, FILE * f3 )
+{
+	/*
+	- f1: acumulador
+	- f2: corrida atual
+	- f3: merge de (f1,f2)
+	*/
+	fseek(f2, 0, SEEK_SET); // joga o 'cursor' para o início do arquivo
+	fseek(f1, 0, SEEK_SET);
+	int lidof1,lidof2, contadorf2=0, contadorf1=-1, memoria =0;
+	while( fread(&lidof2, sizeof(int), 1, f2) == 1 )	// enquanto houver 
+	{
+		contadorf2++;
+		fread(&lidof1, sizeof(int), 1, f1);
+		contadorf1++;
+		if(lidof1<lidof2 && lidof1!= memoria) {
+			
+			fwrite( &lidof1, sizeof(int), 1, f3 );
+			memoria = lidof1;
+			fseek(f2, - sizeof(int), SEEK_CUR);
+			// printf("contadorf1\n");
+		}
+		else if(lidof1>lidof2 && lidof2!= memoria) {
+			fwrite( &lidof2, sizeof(int), 1, f3 );
+			memoria = lidof2;
+			fseek(f1, - sizeof(int), SEEK_CUR);
+			// printf("contadorf1\n");
+		}
+
+		else if (lidof1==lidof2 && lidof1!= memoria){
+			fwrite( &lidof1, sizeof(int), 1, f3);
+			memoria = lidof1;
+			printf("Lido %i %i\n", lidof1, lidof2);
+			printf("contadorfx\n");
+		}
+		// printf("Lido %i %i\n", lidof1, lidof2);
+		// printf("Contador f2: %i\n", contadorf2);
+	}
+
+	while( fread(&lidof1, sizeof(int), 1, f2) == 1 )
+	{
+		fwrite( &lidof1, sizeof(int), 1, f3);
+	}
 }
 
 //------------------------------------------
@@ -91,7 +140,9 @@ void ordenar(char *nomearq)
 
 	while( !feof(arquivo) )	// enquanto "houver arquivo"
 	{	
+		binToBin(final, temp1);
 		corrida++;
+		printf("Corrida: %i\n", corrida);
 		for ( i = 0; i < 100 ; i++ )	// percorre todo o arquivo
 		{
 			if ( ! (fscanf(arquivo, "%i", &M[i] ) == 1) )	// preenche a memória com os dados
@@ -99,13 +150,38 @@ void ordenar(char *nomearq)
 			contador++;		// quantidade de dados lidos
 		}
 		sort( contador );	// ordena a memória
+		gravar_arquivo_tmp(temp2, contador);
+		
+		if(corrida == 1){
+			binToBin(temp2, temp1);
+			//contador=0;
+			//continue;
+			merge(temp2, temp1, final);
+			break;
+		}
+
+		printf("Merge\n");
+		merge(temp1, temp2, final);
 		contador = 0;
 	}
-	fwrite( &M[0], sizeof(int), 10, temp1 );	// escreve o conteúdo da memória no arquivo temporário
-	if ((fread( &M[0], sizeof(int), 1 ,temp1) == 1));
-	gravar_arquivo( saida, 100 );
 
-	binToBin( temp1, temp2 );
+	fseek( final, 0, SEEK_SET );
+	int pavanelli;
+	while ( 1 == (fread(&pavanelli, sizeof(int), 1, final)) )
+	{
+		fprintf(saida, "%i ", pavanelli);
+		// printf("Imprimindo\n");
+	}
+
+
+	//binToTxt()
+	//fwrite( &M[0], sizeof(int), 100, temp1 );	// escreve o conteúdo da memória no arquivo temporário
+	// if ((fread( &M[0], sizeof(int), 1 ,temp1) == 1));
+	// gravar_arquivo( saida, 100 );
+
+	//binToBin( temp1, temp2 );
+
+	 //merge( temp1, temp2, final );
 
 	/*	Próximos Passos
 	 *
