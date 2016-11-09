3137b6ae7eb020b86e77bef56d5441b5eb588446
Matheus Pavanelli <mspavanelli@gmail.com>
2016-11-09 00:29:09 -0200

    Create modelo.py

---
+++ modelo.py
@@ -0,0 +1,39 @@
+# memória útil para a ordenação
+M = [ None for i in range(200) ]
+
+# ordenação da memória: selection sort
+def sort(limite):
+	pass
+
+# =========================== #
+# funções auxiliares de escrita
+# =========================== #
+
+# copia conteúdo de arquivo binário para arquivo texto
+def binToTxT(origem, destino):
+	pass
+
+# Clona um arquivo binário
+def binToBin(origem, destino):
+	pass
+
+# Copia os dados da memória para um arquivo temporário.
+# A variável limite serve para indicar o tamanho da corrida
+def gravar_arquivo_tmp(destino, limite):
+	pass
+
+
+# Intercalação de arquivos utilizando o princípio do Merge Sort
+#	- Arquivo f1 e f2: Arquivos de entrada
+#	- Arquivo f3: Resultado da intercalação de f1 com f2
+def merge(f1, f2, f3)
+	pass
+
+# A lógica usada foi ordenar e já intercalar a cada corrida realizada
+def ordenar(arquivo):
+	pass
+
+
+def main():
+	arquivo = 'exemplo.txt'
+	ordenar(arquivo)
